796bfbc30a5ef925e78d4b951775d2b95b5b328e
Micheil <fouriemicheil1@gmail.com>
2025-04-14 15:20:36 +0200

    chore: prevent scroll indicator from showing on mobile and centering correctly with infoMenu absent

--- src/Components/ScrollIndicator.jsx
+++ src/Components/ScrollIndicator.jsx
@@ -4,7 +4,7 @@ import { faChevronCircleDown } from "@fortawesome/free-solid-svg-icons";
 const ScrollIndicator = ({ showScrollIndicator }) => {
   return (
     <div
-      className={`${showScrollIndicator ? "opacity-100" : "opacity-0"} fixed bottom-0 left-1/2 z-50 mx-auto max-w-screen-xl -translate-x-[calc(50%-150px)] animate-bounce transition-opacity duration-300 ease-in-out`}
+      className={`${showScrollIndicator ? "opacity-100" : "opacity-0"} fixed bottom-0 left-1/2 z-50 mx-auto max-w-screen-xl animate-bounce transition-opacity duration-300 ease-in-out max-md:hidden min-lg:-translate-x-[calc(50%-150px)]`}
     >
       <div className="bg-navy-500 rounded-full">
         <FontAwesomeIcon
